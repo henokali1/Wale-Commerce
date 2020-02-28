42b52adc5ce4d437b73dc351af988cebe3e3e550
henokali1 <henokali1@gmail.com>
2020-02-28 12:51:05 +0000

    Product starter

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.pages, name='index.html'),
+    path('product/', views.product, name='product.html'),
     # path('cats/<str:cat_urls>/', views.cat_urls, name='cat_urls.html'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from .models import *
 import urllib.parse
 
-
+# Home page
 def pages(request):
 	return render(request, 'pages/index.html')
+
+# Product page
+def product(request):
+    args = {}
+    return render(request, 'pages/product.html', args)
