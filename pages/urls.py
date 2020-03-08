5e6789b4dfa7091224902cf10b228c4d628869b7
henokali1 <henokali1@gmail.com>
2020-03-08 07:10:27 +0000

    Add product starter

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.pages, name='index.html'),
     path('product/', views.product, name='product.html'),
+    path('add-product/', views.add_prodcut, name='add-product.html')
     # path('cats/<str:cat_urls>/', views.cat_urls, name='cat_urls.html'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -11,3 +11,12 @@ def pages(request):
 def product(request):
     args = {}
     return render(request, 'pages/product.html', args)
+
+# Add product page
+def add_prodcut(request):
+    args = {}
+    if request.method == 'POST':
+        # new_trainee = EnrollTrainee()
+        title = request.POST['product_title']
+        print(title)
+    return render(request, 'pages/add-product.html', args)
